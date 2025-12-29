ddce501327153f765b7584f53da02c827e8e8963
Mithradevi1303 <mithradevikathirvel2005@gmail.com>
2025-12-29 08:22:49 +0530

    Modified indexstyle js file

---
+++ indexstyle.js
@@ -0,0 +1,28 @@
+function addStudent(event) {
+    event.preventDefault(); // stop page refresh
+
+    let name = document.getElementById("Name").value;
+    let age = document.getElementById("Age").value;
+    let course = document.getElementById("Course").value;
+    let email = document.getElementById("Email").value;
+
+    let gender = document.querySelector('input[name="Gender"]:checked').value;
+
+    let table = document.getElementById("tabledata");
+
+    let row = table.insertRow();
+
+    row.insertCell(0).innerHTML = name;
+    row.insertCell(1).innerHTML = age;
+    row.insertCell(2).innerHTML = course;
+    row.insertCell(3).innerHTML = email;
+    row.insertCell(4).innerHTML =
+        '<button class="delete-btn" onclick="deleteRow(this)">Delete</button>';
+
+    document.getElementById("Form").reset();
+}
+
+function deleteRow(btn) {
+    let row = btn.parentNode.parentNode;
+    row.remove();
+}
